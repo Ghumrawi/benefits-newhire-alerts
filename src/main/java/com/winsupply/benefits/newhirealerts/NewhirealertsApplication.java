ec3db898b2915815821d4a634a7c57810fe1dfb7
akghumrawi <akghumrawi@winsupplyinc.com>
2022-05-24 10:12:54 -0400

    Initial commit

---
+++ src/main/java/com/winsupply/benefits/newhirealerts/NewhirealertsApplication.java
@@ -0,0 +1,13 @@
+package com.winsupply.benefits.newhirealerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewhirealertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewhirealertsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winsupply/benefits/newhirealerts/NewhirealertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winsupply.benefits.newhirealerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewhirealertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
